ab6633539f81d240cf4923131c94f716c5873a43
Jurgen Kurz <jurgen.kurz@gmail.com>
2026-03-18 05:13:15 +0100

    feat: add TypeTransaction enum (depense|recette)

---
+++ app/Enums/TypeTransaction.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace App\Enums;
+
+enum TypeTransaction: string
+{
+    case Depense = 'depense';
+    case Recette = 'recette';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Depense => 'Dépense',
+            self::Recette => 'Recette',
+        };
+    }
+}
