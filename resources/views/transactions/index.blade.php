1ce6bc1ed894ed2b21250fa0848951b8726d21db
Jurgen Kurz <jurgen.kurz@gmail.com>
2026-03-21 11:34:15 +0100

    fix(lot3): remet les composants import-csv sur la page transactions

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -1,6 +1,10 @@
 <x-app-layout>
     <div class="container-fluid py-3">
         <h4 class="mb-3"><i class="bi bi-list-ul me-2"></i>Transactions</h4>
+        <div class="mb-3">
+            <livewire:import-csv type="depense" />
+            <livewire:import-csv type="recette" />
+        </div>
         <livewire:transaction-universelle :locked-types="['depense', 'recette']" />
     </div>
 </x-app-layout>
