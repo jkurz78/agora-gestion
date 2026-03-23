4c42e78812e65ecb97935afc3b1ca1339dc94872
Jurgen Kurz <jurgen.kurz@gmail.com>
2026-03-23 07:38:40 +0100

    feat(lot4): integrate sync config and sync launch components in HelloAsso settings

--- resources/views/parametres/helloasso.blade.php
+++ resources/views/parametres/helloasso.blade.php
@@ -2,6 +2,8 @@
     <div class="container py-3">
         <h1 class="mb-4">Connexion HelloAsso</h1>
         <livewire:parametres.helloasso-form />
+        <livewire:parametres.helloasso-sync-config />
         <livewire:parametres.helloasso-tiers-rapprochement />
+        <livewire:parametres.helloasso-sync />
     </div>
 </x-app-layout>
